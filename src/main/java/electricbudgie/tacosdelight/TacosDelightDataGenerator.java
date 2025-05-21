9428a9bdf84bff1daf51ba34250921f8bc06fe73
ElectricBudgie <lkdohse@gmail.com>
2025-05-20 22:00:39 -0400

    initial commit

---
+++ src/main/java/electricbudgie/tacosdelight/TacosDelightDataGenerator.java
@@ -0,0 +1,11 @@
+package electricbudgie.tacosdelight;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TacosDelightDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
