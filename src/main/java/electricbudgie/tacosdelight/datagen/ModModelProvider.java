8a114df68672082f6300bca1fd7ecfd5ffb67427
ElectricBudgie <lkdohse@gmail.com>
2025-05-22 12:05:51 -0400

    refactored tall plant model creation

--- src/main/java/electricbudgie/tacosdelight/datagen/ModModelProvider.java
+++ src/main/java/electricbudgie/tacosdelight/datagen/ModModelProvider.java
@@ -8,9 +8,12 @@ import electricbudgie.tacosdelight.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.block.Block;
+import net.minecraft.block.enums.DoubleBlockHalf;
 import net.minecraft.data.client.*;
 import net.minecraft.state.property.Properties;
 import net.minecraft.state.property.Property;
+
+import java.util.Map;
 
 public class ModModelProvider extends FabricModelProvider {
     public ModModelProvider(FabricDataOutput output) {
@@ -41,11 +44,18 @@ public class ModModelProvider extends FabricModelProvider {
             };
         });
         generator.blockStateCollector.accept(VariantsBlockStateSupplier.create(plant).coordinate(blockStateVariantMap));
-        ageProperty.getValues().forEach(value -> {
-            Properties.DOUBLE_BLOCK_HALF.getValues().forEach(blockHalf -> {
-                generator.createSubModel(plant, "_age" + value.toString() + "_" + blockHalf.name().toLowerCase(), Models.CROSS, TextureMap::cross);
-            });
-        });
-
+        ageProperty.getValues().stream()
+                        .flatMap(age -> Properties.DOUBLE_BLOCK_HALF.getValues().stream()
+                                .map(half -> Map.entry(age, half)))
+                                .forEach(entry -> {
+                                    Integer age = entry.getKey();
+                                    DoubleBlockHalf half = entry.getValue();
+                                    generator.createSubModel(
+                                            plant,
+                                            "_age" + age + "_" + half.name().toLowerCase(),
+                                            Models.CROSS,
+                                            TextureMap::cross
+                                    );
+                                });
     }
 }
