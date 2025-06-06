8723c033b473522233a8bdff7e2203f9121d5310
ElectricBudgie <lkdohse@gmail.com>
2025-06-05 22:59:34 -0400

    the oil in the deep fryer is now translucent

--- src/main/java/electricbudgie/tacosdelight/block/entity/renderer/DeepFryerBlockEntityRenderer.java
+++ src/main/java/electricbudgie/tacosdelight/block/entity/renderer/DeepFryerBlockEntityRenderer.java
@@ -1,8 +1,13 @@
 package electricbudgie.tacosdelight.block.entity.renderer;
 
+import electricbudgie.tacosdelight.TacosDelight;
 import electricbudgie.tacosdelight.block.entity.custom.DeepFryerBlockEntity;
 import electricbudgie.tacosdelight.block.entity.renderer.client.DeepFryerModel;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.renderer.GeoBlockRenderer;
 
 public class DeepFryerBlockEntityRenderer extends GeoBlockRenderer<DeepFryerBlockEntity> {
@@ -10,5 +15,8 @@ public class DeepFryerBlockEntityRenderer extends GeoBlockRenderer<DeepFryerBloc
         super(new DeepFryerModel());
     }
 
-
+    @Override
+    public @Nullable RenderLayer getRenderType(DeepFryerBlockEntity animatable, Identifier texture, @Nullable VertexConsumerProvider bufferSource, float partialTick) {
+        return RenderLayer.getEntityTranslucentCull(Identifier.of(TacosDelight.MOD_ID, "textures/block/entity/deep_fryer.png"));
+    }
 }
