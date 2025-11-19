5356fbb5602e9eb8b58279e0f6939a616bf90b34
ElectricBudgie <lkdohse@gmail.com>
2025-11-19 13:16:06 -0500

    added more food

--- src/main/java/electricbudgie/tacosdelight/item/ModItems.java
+++ src/main/java/electricbudgie/tacosdelight/item/ModItems.java
@@ -35,6 +35,7 @@ public class ModItems {
     public static final Item TORTILLA_CHIPS = registerItemViaSettings("tortilla_chips", ModFoodComponents.TORTILLA_CHIPS);
     public static final Item FLOUR_TOSTADA = registerItemViaSettings("flour_tostada", ModFoodComponents.FLOUR_TOSTADA);
     public static final Item CHEESE_WEDGE = registerItemViaSettings("cheese_wedge", ModFoodComponents.CHEESE_WEDGE);
+    public static final Item HAND_PIE_CRUST = registerItemViaSettings("hand_pie_crust", ModFoodComponents.HAND_PIE_CRUST_SETTINGS);
 
     //Raw Ingredients
     public static final Item RAW_TACO_BEEF = registerItemViaSettings("raw_taco_beef", ModFoodComponents.RAW_TACO_BEEF_SETTINGS);
@@ -54,10 +55,22 @@ public class ModItems {
     public static final Item UNCOOKED_CHICKEN_CRUNCHWRAP = registerItemViaSettings("uncooked_chicken_crunchwrap", ModFoodComponents.UNCOOKED_CHICKEN_CRUNCHWRAP_SETTINGS);
     public static final Item UNCOOKED_BEEF_CRUNCHWRAP = registerItemViaSettings("uncooked_beef_crunchwrap", ModFoodComponents.UNCOOKED_BEEF_CRUNCHWRAP_SETTINGS);
     public static final Item UNCOOKED_POTATO_CRUNCHWRAP = registerItemViaSettings("uncooked_potato_crunchwrap", ModFoodComponents.UNCOOKED_POTATO_CRUNCHWRAP_SETTINGS);
+    public static final Item TORTILLA_WITH_CHEESE= registerItemViaSettings("tortilla_with_cheese", ModFoodComponents.TORTILLA_WITH_CHEESE_SETTINGS);
+    public static final Item UNCOOKED_CHEESY_POTATO_GRILLER= registerItemViaSettings("uncooked_cheesy_potato_griller", ModFoodComponents.UNCOOKED_CHEESY_POTATO_GRILLER_SETTINGS);
+    public static final Item UNCOOKED_CHICKEN_TAQUITO = registerItemViaSettings("uncooked_chicken_taquito", ModFoodComponents.UNCOOKED_CHICKEN_TAQUITO_SETTINGS);
+    public static final Item UNCOOKED_STEAK_TAQUITO = registerItemViaSettings("uncooked_steak_taquito", ModFoodComponents.UNCOOKED_STEAK_TAQUITO_SETTINGS);
+    public static final Item UNCOOKED_CARAMEL_APPLE_EMPANADA = registerItemViaSettings("uncooked_caramel_apple_empanada", ModFoodComponents.UNCOOKED_CARAMEL_APPLE_EMPANADA);
 
     //Sides
     public static final Item NACHOS = registerItemViaSettings("nachos", ModFoodComponents.NACHOS_SETTINGS);
     public static final Item CHEESY_FIESTA_POTATOES = registerItemViaSettings("cheesy_fiesta_potatoes", ModFoodComponents.CHEESY_FIESTA_POTATOES_SETTINGS);
+    public static final Item CARAMEL_APPLE_EMPANADA = registerItemViaSettings("caramel_apple_empanada", ModFoodComponents.CARAMEL_APPLE_EMPANADA_SETTINGS);
+
+    //Roll-Ups
+    public static final Item CHEESY_ROLL_UP = registerItemViaSettings("cheesy_roll_up", ModFoodComponents.CHEESY_ROLL_UP_SETTINGS);
+    public static final Item CHEESY_POTATO_GRILLER = registerItemViaSettings("cheesy_potato_griller", ModFoodComponents.CHEESY_POTATO_GRILLER_SETTINGS);
+    public static final Item CHICKEN_TAQUITO = registerItemViaSettings("chicken_taquito", ModFoodComponents.CHICKEN_TAQUITO_SETTINGS);
+    public static final Item STEAK_TAQUITO = registerItemViaSettings("steak_taquito", ModFoodComponents.STEAK_TAQUITO_SETTINGS);
 
     //Tacos
     public static final Item POTATO_TACO = registerItemViaSettings("potato_taco", ModFoodComponents.POTATO_TACO_SETTINGS);
